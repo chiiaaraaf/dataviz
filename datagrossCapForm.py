f256623b4cb4e15307015bfbc8434f12b5f9a0dc
chiiaaraaf <89153535+chiiaaraaf@users.noreply.github.com>
2024-01-11 14:13:15 +0100

    Update datagrossCapForm.py

--- datagrossCapForm.py
+++ datagrossCapForm.py
@@ -29,7 +29,7 @@ for country in data_long['Country Name'].unique():
         )
     )
 
-buttons = [dict(label='Select', method='update', 
+buttons = [dict(method='update', 
                 args=[{'visible': [False]*len(data_long['Country Name'].unique())},
                       {'title': 'Select a country to display the data'}])]
 
@@ -48,7 +48,7 @@ fig.update_layout(
         'type': 'dropdown',
         'buttons': buttons,
         'direction': 'down',
-        'active': 1,  # No country is selected by default
+        'active': -1,  # No country is selected by default
         'pad': {'r': 10, 't': 10},
         'x': 0.01,
         'xanchor': 'left',
